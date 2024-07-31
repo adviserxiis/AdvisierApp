ba41d47818e01bee3e7015656c2ecf6c9adb081a
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2024-07-31 13:58:06 +0530

    abb file generated

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,51 +8,45 @@ import {
   Pressable,
   StatusBar,
   View,
-  TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
-import firebase from '../../api/firebase';
-import {login, sendOTP} from '../../api/auth';
-import {useNavigation} from '@react-navigation/native';
-import auth from '@react-native-firebase/auth';
-import {storeData} from '../../utils/store';
-import {setUser} from '../../features/user/userSlice';
-import {useDispatch} from 'react-redux';
-import {GoogleSignin, statusCodes} from '@react-native-google-signin/google-signin'
-
-
-
+import { useNavigation } from '@react-navigation/native';
+import { storeData } from '../../utils/store';
+import { setUser } from '../../features/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+import { sendOTP } from '../../api/auth';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 const Login = () => {
   const navigation = useNavigation();
   const [number, setNumber] = useState('');
   const [error, setError] = useState('');
+  const [confirmation, setConfirmation] = useState(null);
+  const [loading, setLoading] = useState(false); // State for loading
   const dispatch = useDispatch();
-  const [confirmation, setConfirmation] = useState(null); 
 
   const validatePhoneNumber = phone => {
     const phoneRegex = /^[0-9]{10}$/;
     return phoneRegex.test(phone);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     GoogleSignin.configure({
-      webClientId:'553796556466-btiglu1cssg04entlq545n5bknsuqdef.apps.googleusercontent.com'
-    })
-  },[])
+      webClientId: '553796556466-btiglu1cssg04entlq545n5bknsuqdef.apps.googleusercontent.com',
+    });
+  }, []);
 
   const onSubmitHandler = async (number: String) => {
-    // const res = await login(number);
-    // console.log('res', res);
     if (!validatePhoneNumber(number)) {
       setError('Please enter a valid 10-digit phone number.');
       return;
     }
     setError('');
 
-    console.log('check');
     try {
       const formattedNumber = `+91${number}`;
-      const confirmationResult = await sendOTP(formattedNumber); 
-      setConfirmation(confirmationResult); // Store the confirmation result
+      const confirmationResult = await sendOTP(formattedNumber);
+      setConfirmation(confirmationResult);
       storeData('mobile_number', formattedNumber);
       dispatch(setUser(formattedNumber));
       navigation.navigate('Otp', { phoneNumber: formattedNumber, confirmation: confirmationResult });
@@ -64,16 +58,15 @@ const Login = () => {
     setNumber('');
   };
 
-  const signIn = async()=>{
-    try{
+  const signIn = async () => {
+    setLoading(true); // Start loading indicator
+    try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
       console.log(userInfo);
-      dispatch(setUser(userInfo))
-      navigation.navigate('Thanks');   
-
-    }
-    catch(error){
+      dispatch(setUser(userInfo));
+      navigation.navigate('Thanks');
+    } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         console.log('User Cancelled the Login Flow');
       } else if (error.code === statusCodes.IN_PROGRESS) {
@@ -81,117 +74,84 @@ const Login = () => {
       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
         console.log('Play Services Not Available');
       } else {
-        console.log(error,'Some Other Error Happened');
+        console.log(error, 'Some Other Error Happened');
       }
+    } finally {
+      setLoading(false); // Stop loading indicator
     }
-  }
+  };
 
   return (
     <SafeAreaView style={styles.outerContainer}>
       <StatusBar barStyle="light-content" backgroundColor="black" />
-      <View style={{
-        alignItems:'center',
-        justifyContent:'center',
-        flex:1,
-        paddingHorizontal:20
-      }}>
-
-      <Image source={require('../../assets/images/logo.png')}  style={{
-        width: 150,
-        height: 60,
-        objectFit:'contain'
-      }}/>
-      <Text style={styles.heading}>Sign in or create Account</Text>
-      <Text style={styles.subHeading}>
-        Hello! Looks like you’re enjoying our page, but you haven’t signed up
-        for an account yet.
-      </Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          value={number}
-          onChangeText={number => {
-            setNumber(number);
-            if (error) setError('');
-          }}
-          placeholderTextColor="#FFFFFF"
-          placeholder="Phone Number"
-          keyboardType="phone-pad"
-        />
-        <Image
-          source={require('../../assets/images/user.png')}
-          style={styles.userImage}
-        />
-      </View>
-      {error ? <Text style={styles.errorText}>{error}</Text> : null}
-      <Pressable
-        style={styles.loginBtnContainer}
-        onPress={() => onSubmitHandler(number)}>
-        <Text style={styles.loginTxt}>Login</Text>
-      </Pressable>
-      <View style={styles.dividerContainer}>
-        <View style={styles.Fdivider} />
-        <Text
-          style={{
-            fontSize: 16,
-            color: '#FFFFFF',
-            marginHorizontal: 10,
-            fontFamily: 'Poppins-Regular',
-          }}>
-          or
+      <View style={styles.innerContainer}>
+        <Image source={require('../../assets/images/logo.png')} style={styles.logo} />
+        <Text style={styles.heading}>Sign in or create Account</Text>
+        <Text style={styles.subHeading}>
+          Hello! Looks like you’re enjoying our page, but you haven’t signed up
+          for an account yet.
         </Text>
-        <View style={styles.Sdivider} />
-      </View>
-      <TouchableOpacity
-        style={{
-          width: '100%',
-          padding: 18,
-          backgroundColor: '#F7F7F7',
-          borderRadius: 10,
-          alignItems: 'center',
-          marginVertical: 10,
-        }} onPress={signIn}>
-        <Text
-          style={{
-            fontSize: 16,
-            color: 'black',
-            fontFamily: 'Poppins-Regular',
-            fontWeight: '700',
-          }}>
-          Sign In with Google
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={number}
+            onChangeText={number => {
+              setNumber(number);
+              if (error) setError('');
+            }}
+            placeholderTextColor="#FFFFFF"
+            placeholder="Phone Number"
+            keyboardType="phone-pad"
+          />
+          <Image source={require('../../assets/images/user.png')} style={styles.userImage} />
+        </View>
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+        <Pressable style={styles.loginBtnContainer} onPress={() => onSubmitHandler(number)}>
+          <Text style={styles.loginTxt}>Login</Text>
+        </Pressable>
+        <View style={styles.dividerContainer}>
+          <View style={styles.Fdivider} />
+          <Text style={styles.orText}>or</Text>
+          <View style={styles.Sdivider} />
+        </View>
+        <Pressable style={styles.googleBtnContainer} onPress={signIn}>
+          {loading ? (
+            <ActivityIndicator size="small" color="#000000" />
+          ) : (
+            <View style={styles.googleBtnContent}>
+              <AntDesign name="google" size={20} color="#000000" style={styles.googleLogo} /> 
+              <Text style={styles.googleBtnText}>Sign In with Google</Text>
+            </View>
+          )}
+        </Pressable>
+        <Text style={styles.bottomTxt}>
+          by creating an account, you agree to our’s{' '}
+          <Text style={styles.underlineText}>Privacy Policy</Text> and{' '}
+          <Text style={styles.underlineText}>Terms of Use.</Text>
         </Text>
-      </TouchableOpacity>
-      <Text style={styles.bottomTxt}>
-        by creating an account, you agree to our’s{' '}
-        <Text
-          style={{
-            textDecorationLine: 'underline',
-          }}>
-          Privacy Policy
-        </Text>{' '}
-        and{' '}
-        <Text
-          style={{
-            textDecorationLine: 'underline',
-          }}>
-          Terms of Use.
-        </Text>
-      </Text>
       </View>
     </SafeAreaView>
   );
 };
-
-export default Login;
 
 const styles = StyleSheet.create({
   outerContainer: {
     backgroundColor: 'black',
     flex: 1,
   },
+  innerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 150,
+    height: 60,
+    objectFit: 'contain',
+  },
   heading: {
     color: '#FFFFFF',
-    // fontWeight: '700',
     fontSize: 20,
     textAlign: 'center',
     marginTop: 20,
@@ -205,7 +165,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Medium',
     marginVertical: 10,
     padding: 10,
-    width:'110%',
+    width: '110%',
     marginHorizontal: 10,
   },
   input: {
@@ -224,7 +184,7 @@ const styles = StyleSheet.create({
     padding: 15,
     marginVertical: 20,
     borderRadius: 10,
-    width:'100%'
+    width: '100%',
   },
   loginTxt: {
     textAlign: 'center',
@@ -240,7 +200,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     textAlign: 'center',
     fontFamily: 'Poppins-Regular',
-    marginTop:10,
+    marginTop: 10,
   },
   errorText: {
     color: 'red',
@@ -275,4 +235,40 @@ const styles = StyleSheet.create({
     gap: 10,
     marginBottom: 10,
   },
+  orText: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    marginHorizontal: 10,
+    fontFamily: 'Poppins-Regular',
+  },
+  googleBtnContainer: {
+    width: '100%',
+    padding: 18,
+    backgroundColor: '#F7F7F7',
+    borderRadius: 10,
+    alignItems: 'center',
+    marginVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  googleBtnContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  googleLogo: {
+    width: 20,
+    height: 20,
+    marginRight: 7,
+  },
+  googleBtnText: {
+    fontSize: 16,
+    color: 'black',
+    fontFamily: 'Poppins-Regular',
+    fontWeight: '700',
+  },
+  underlineText: {
+    textDecorationLine: 'underline',
+  },
 });
+
+export default Login;
